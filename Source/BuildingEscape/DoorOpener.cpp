9f69bef5ccbd7a0cd5fbbc415c82f97f983cbf29
jrpavoncello <jrpavoncello@gmail.com>
2019-08-10 20:07:15 -0400

    Built out the starting room, added starting code for triggering a door open

--- Source/BuildingEscape/DoorOpener.cpp
+++ Source/BuildingEscape/DoorOpener.cpp
@@ -1,11 +1,12 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "PositionReporter.h"
+#include "DoorOpener.h"
 #include "GameFramework/Actor.h"
+#include "Quat.h"
 
 // Sets default values for this component's properties
-UPositionReporter::UPositionReporter()
+UDoorOpener::UDoorOpener()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
@@ -16,22 +17,21 @@ UPositionReporter::UPositionReporter()
 
 
 // Called when the game starts
-void UPositionReporter::BeginPlay()
+void UDoorOpener::BeginPlay()
 {
 	Super::BeginPlay();
 
-	auto objectName = GetOwner()->GetName();
-	auto location = GetOwner()->GetActorTransform().GetLocation();
-
-	UE_LOG(LogTemp, Warning, TEXT("Position reporter started for %s"), *objectName);
-	UE_LOG(LogTemp, Warning, TEXT("%s is at X=%f, Y=%f, Z=%f"), *objectName, location.X, location.Y, location.Z);
-	
+	// ...
+	auto owner = GetOwner();
+	owner->SetActorRelativeRotation(FRotator(0, 90, 0));
 }
 
+
 // Called every frame
-void UPositionReporter::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+void UDoorOpener::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
+	// ...
 }
 
---
+++ Source/BuildingEscape/DoorOpener.h
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "Engine/TriggerVolume.h"
+#include "DoorOpener.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class BUILDINGESCAPE_API UDoorOpener : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UDoorOpener();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+	UPROPERTY(EditAnywhere)
+	float OpenAngle = 90.f;
+	UPROPERTY(EditAnywhere)
+	ATriggerVolume* PressurePlate;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+		
+};
